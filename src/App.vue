1521cf1c776c5f7b782c3634da588accc93ac422
itcast <itheima@itcast.cn>
2022-06-03 21:10:07 +0800

    引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,23 @@
     <!-- 路由的出口 -->
     <router-view />
     <h1>黑马头条</h1>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <hr />
+    <van-button icon="plus" type="primary" />
+    <van-button icon="plus" type="primary">按钮</van-button>
+    <van-button
+      icon="https://fastly.jsdelivr.net/npm/@vant/assets/user-active.png"
+      type="primary"
+    >
+      按钮
+    </van-button>
   </div>
 </template>
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 <style lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+//加载Vant核心组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+//加载全局样式
 import './styles/index.less'
+import './styles/icon.less'
+//注册使用Vant使用库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
